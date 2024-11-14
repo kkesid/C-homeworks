fe1ca9d18c307710b9cf89659ce4abef5db160c2
kkesid <160251018+kkesid@users.noreply.github.com>
2024-11-14 21:56:25 +0300

    Add files via upload

---
+++ hw1_ex3.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое число N (больше 0): ");
+
+int N = int.Parse(Console.ReadLine());
+string reversedNumber = ReverseNumber(N);
+Console.WriteLine("Число, прочитанное справа налево: " + reversedNumber);
+
+static string ReverseNumber(int number)
+{
+    char[] digits = number.ToString().ToCharArray(); 
+    Array.Reverse(digits); 
+    return new string(digits);
+}
+
+        
