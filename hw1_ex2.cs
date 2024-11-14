651fcd752e6b2fdd87f44beadd5f21e181322d9f
kkesid <160251018+kkesid@users.noreply.github.com>
2024-11-14 21:56:11 +0300

    Add files via upload

---
+++ hw1_ex2.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter A: ");
+string A = Console.ReadLine();
+Console.WriteLine("Enter B: ");
+string B = Console.ReadLine();
+
+int a = int.Parse(A);
+int b = int.Parse(B);
+
+for (int i = a; i <= b; ++i)
+{
+    for (int j = 0; j < i; ++j)
+    {
+        Console.Write(i);
+
+    }
+    Console.Write("\n");
+}
