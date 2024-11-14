786e65992a033d07e2d33445b2812c40d7ce7010
kkesid <160251018+kkesid@users.noreply.github.com>
2024-11-14 23:29:11 +0300

    Add files via upload

---
+++ hw2_ex5.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите строку:");
+string text = Console.ReadLine();
+
+string[] words = text.Split(' ');
+
+Console.WriteLine($"Количество слов: {words.Length}");
+Console.WriteLine("Список слов:");
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
