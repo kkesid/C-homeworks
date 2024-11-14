955deccc2a2ddc53a5dcac2384fed54d8969b811
kkesid <160251018+kkesid@users.noreply.github.com>
2024-11-14 23:28:47 +0300

    Add files via upload

---
+++ hw2_ex4.cs
@@ -0,0 +1,5 @@
+﻿string todayDate = DateTime.Now.ToShortDateString();
+TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
+Console.WriteLine($"Сегодня: {todayDate}");
+Console.WriteLine($"Текущее время: {currentTime}");
