acd1785f6b4d8f710197a13c3155130b2e2ddc21
kkesid <160251018+kkesid@users.noreply.github.com>
2024-11-14 23:28:04 +0300

    Add files via upload

---
+++ hw2_ex2.cs
@@ -0,0 +1,17 @@
+﻿
+int[] numbers = { 1, -2, 3, -4, 5, -6, 7, 8, -9, 10, 11, -12, 13, 14, -15 };
+
+int num1 = 0, num2 = 0;
+
+foreach (var number in numbers)
+{
+    if (number>0)
+        num1 += number;
+    else
+        num2 += number;
+}
+
+Console.WriteLine("Сумма положительных чисел равна: "+num1);
+Console.WriteLine("Сумма отрицательных чисел равна: "+num2);
+
+
