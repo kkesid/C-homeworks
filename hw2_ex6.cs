494eb61711215fda788423f4b5c8272dbbc937d9
kkesid <160251018+kkesid@users.noreply.github.com>
2024-11-14 23:29:34 +0300

    Add files via upload

---
+++ hw2_ex6.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+Console.WriteLine("Введите строку:");
+string inputString = Console.ReadLine();
+
+StringBuilder newString = new StringBuilder();
+
+foreach (char c in inputString)
+{
+    if (char.IsDigit(c))
+    {
+        int number = int.Parse(c.ToString());
+        string word = ((Number)number).ToString();
+
+        newString.Append(word);
+        newString.Append(' ');
+    }
+    else
+    {
+        newString.Append(c);
+    }
+}
+
+Console.WriteLine(newString.ToString());
+
+public enum Number
+{
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine
+}
+
