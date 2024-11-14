1ad3c3fa0e2b66a91e2a403767644b83473107cb
kkesid <160251018+kkesid@users.noreply.github.com>
2024-11-14 23:28:26 +0300

    Add files via upload

---
+++ hw2_ex3.cs
@@ -0,0 +1,31 @@
+﻿List<int> numbers = new List<int>{ 1, 4, 5, 4, 7, 8, 1, 2, 4, 9, 1, 5, 10, 2, 2 };
+
+Dictionary<int, int> counts = new Dictionary<int, int>();
+List<int> duplicates = new List<int>();
+
+foreach (int number in numbers)
+{
+    if (counts.ContainsKey(number))
+    {
+        counts[number]++;
+    }
+    else
+    {
+        counts[number] = 1;
+    }
+}
+
+foreach (KeyValuePair<int, int> kvp in counts)
+{
+    if (kvp.Value > 1)
+    {
+        duplicates.Add(kvp.Key);
+    }
+}
+
+Console.WriteLine("Повторяющиеся числа:");
+foreach (int num in duplicates)
+{
+    Console.Write(num + " ");
+}
+
