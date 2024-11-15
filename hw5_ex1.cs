70e83a33ac5f2ef407d7751a0fc32d007cc97d83
kkesid <160251018+kkesid@users.noreply.github.com>
2024-11-15 23:13:46 +0300

    Add files via upload

---
+++ hw5_ex1.cs
@@ -0,0 +1,75 @@
+﻿
+Printable[] printables = new Printable[]
+        {
+            new Book("Pride and Prejudice"),
+            new Magazine("Vogue"),
+            new Book("War and Peace"),
+            new Magazine("Cosmopolitan")
+        };
+
+foreach (var printable in printables)
+{
+    printable.Print();
+}
+
+Console.WriteLine("\nPrinting only magazines:");
+Magazine.PrintMagazines(printables);
+
+Console.WriteLine("\nPrinting only books:");
+Book.PrintBooks(printables);
+
+public interface Printable
+{
+    void Print();
+}
+
+public class Book : Printable
+{
+    public string Title { get; set; }
+
+    public Book(string title)
+    {
+        Title = title;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Book: {Title}");
+    }
+
+    public static void PrintBooks(Printable[] printables)
+    {
+        foreach (var printable in printables)
+        {
+            if (printable is Book book)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
+public class Magazine : Printable
+{
+    public string Title { get; set; }
+
+    public Magazine(string title)
+    {
+        Title = title;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Magazine: {Title}");
+    }
+
+    public static void PrintMagazines(Printable[] printables)
+    {
+        foreach (var printable in printables)
+        {
+            if (printable is Magazine magazine)
+            {
+                Console.WriteLine(magazine.Title);
+            }
+        }
+    }
+}
